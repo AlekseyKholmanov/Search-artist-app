9e3ee665bfcb4f10d5a5f2c1845e8e3a779b47ef
AlekseyKholmanov <a.holmanov@gmail.com>
2019-08-25 20:54:44 +0300

    change webViewClien
    if user click on element in opened page, he are going to browser.

--- app/src/main/java/ru/aholmanov/search_artist_app/ui/searchResult/SearchResultActivity.kt
+++ app/src/main/java/ru/aholmanov/search_artist_app/ui/searchResult/SearchResultActivity.kt
@@ -2,8 +2,8 @@ package ru.aholmanov.search_artist_app.ui.searchResult
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.webkit.WebChromeClient
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_search_result.*
 import ru.aholmanov.search_artist_app.R
@@ -22,21 +22,17 @@ class SearchResultActivity : AppCompatActivity() {
         super.onResume()
         view.settings.javaScriptEnabled = true
         view.alpha = 0.0f
-        view.webChromeClient = object : WebChromeClient() {
-            override fun onProgressChanged(view: WebView?, newProgress: Int) {
-                super.onProgressChanged(view, newProgress)
-                val progress = newProgress * 100
-                progressBar.progress = progress
-                if (newProgress > 99){
-                    progressBar.animate().alpha(0.0f).duration = 800
-                    view?.animate()?.alpha(1.0f)?.duration = 1000
-                }
+        view.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressBar.animate().alpha(0.0f).duration = 800
+                view?.animate()?.alpha(1.0f)?.duration = 1000
             }
         }
         loadUrl()
     }
 
-    private fun loadUrl(){
+    private fun loadUrl() {
         val url = intent.extras?.getString("url") ?: ""
         view.loadUrl(url)
     }
