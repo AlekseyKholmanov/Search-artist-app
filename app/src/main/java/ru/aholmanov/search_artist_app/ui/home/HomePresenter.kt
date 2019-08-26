5ff621c4c7cd59bb86656bef5ca1a9b5a5be9469
AlekseyKholmanov <a.holmanov@gmail.com>
2019-08-27 02:40:06 +0300

    create observable on every query

--- app/src/main/java/ru/aholmanov/search_artist_app/ui/home/HomePresenter.kt
+++ app/src/main/java/ru/aholmanov/search_artist_app/ui/home/HomePresenter.kt
@@ -2,11 +2,11 @@ package ru.aholmanov.search_artist_app.ui.home
 
 import android.annotation.SuppressLint
 import android.util.Log
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import io.reactivex.subjects.PublishSubject
 import ru.aholmanov.search_artist_app.App
-import ru.aholmanov.search_artist_app.model.Artist
+import ru.aholmanov.search_artist_app.R
 import ru.aholmanov.search_artist_app.repository.ArtistRepository
 import ru.aholmanov.search_artist_app.repository.PreferenceRepository
 import java.util.concurrent.TimeUnit
@@ -17,35 +17,38 @@ class HomePresenter(private val view: HomeView) {
         App.component.inject(this)
     }
 
+    fun sendNextText(word: String) {
+        observable = Observable.just(word)
+        search()
+    }
+
     @SuppressLint("CheckResult")
-    fun subscribeOnSearch() {
-        subject
+    private fun search() {
+        observable!!
             .filter { it.isNotEmpty() }
             .debounce(500, TimeUnit.MILLISECONDS)
             .distinctUntilChanged()
             .switchMap {
                 artistRepository.getArtists(it)
                     .subscribeOn(Schedulers.io())
-                    .doOnError { Log.d(TAG, it.localizedMessage) }
+                    .doOnError { Log.d(tag, it.localizedMessage) }
             }
             .map { it.results.artistamches.artist }
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({ responce ->
-                view.updateDropdown(responce)
+                if (responce.isEmpty())
+                    view.showEmptyPlaceholder()
+                else
+                    view.showArtists(responce)
             }, {
-                Log.d(TAG, it.localizedMessage)
-                view.showError("unexpectedError")
+                view.showError(R.string.error_search)
             })
     }
 
-    fun sendNextText(word: String) {
-        subject.onNext(word)
-    }
+    private var observable: Observable<String>? = null
 
-    private val subject by lazy { PublishSubject.create<String>() }
-
-    private val TAG = this.javaClass.canonicalName
+    private val tag = this.javaClass.canonicalName
     @Inject
     lateinit var artistRepository: ArtistRepository
     @Inject
